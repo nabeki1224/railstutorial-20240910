71528cf8799f62b7d43acd19a7b85cde4f912fd4
nabeki1224 <watanabe.kyo19991224@gmail.com>
2024-09-13 11:59:23 +0900

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,4 +20,12 @@ User.create!(name:  "Kyo Watanabe",
                email: email,
                password:              password,
                password_confirmation: password)
+
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content)}
+end
+              
