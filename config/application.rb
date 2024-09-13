b3c472b31cdf2e2737b10251057c8f04ed9cf8e9
nabeki1224 <watanabe.kyo19991224@gmail.com>
2024-09-13 17:53:28 +0900

    Finished All

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,10 @@
 require_relative "boot"
-
 require "rails/all"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
     config.load_defaults 7.0
-    config.active_storage.valiant_processor = :mini_magick
+    config.active_storage.variant_processor = :mini_magick
   end
 end
