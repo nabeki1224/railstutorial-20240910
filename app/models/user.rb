8edd3664b50ac29080a47db6cb20f0f8c830a98e
nabeki1224 <watanabe.kyo19991224@gmail.com>
2024-09-13 16:35:28 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  
+  has_many :followers, through: :passive_relationships, source: :follower
+  # followersは単数系がそのままfolowerなので、『, source: :follower』はなくてもよい
+
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
@@ -40,6 +51,21 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id).includes(:user, image_attachment: :blob)
+    # following_ids -> 自動でidを列挙するメソッド
+    # でも処理する配列が膨大で重いので、SQLとして処理してDBに分担する...?
+  end
+
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,11 @@
-# メインのサンプルユーザーを1人作成する
+# Users
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobarbaz",
              password_confirmation: "foobarbaz",
-             admin: true)
+             admin:     true)
 
-User.create!(name:  "Kyo Watanabe",
-             email: "watanabe.kyo19991224@gmail.com",
-             password:              "aaaaaaaa",
-             password_confirmation: "aaaaaaaa",
-             admin: true)
-
-# 追加のユーザーをまとめて生成する
-99.times do |n|
+300.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -20,12 +13,39 @@ User.create!(name:  "Kyo Watanabe",
                email: email,
                password:              password,
                password_confirmation: password)
-
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.microposts.create!(content: content)}
+  users.each { |user| user.microposts.create!(content: content) }
 end
-              
+
+# ユーザーフォローのリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+
+
+
+User.create!(name:  "Kyo Watanabe",
+             email: "watanabe.kyo19991224@gmail.com",
+             password:              "aaaaaaaa",
+             password_confirmation: "aaaaaaaa",
+             admin:     true)
+
+watanabe = User.find_by(name: "Kyo Watanabe")
+100.times do |n|
+  other_following = User.find(n+1)
+  other_followers = User.find(n+101)
+
+  unless(other_following == watanabe)
+    watanabe.follow(other_following)
+    other_followers.follow(watanabe)
+  end
+end
+    
